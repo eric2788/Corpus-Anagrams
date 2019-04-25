2df8bef809236e76365adc3cdd8a0cef328cebca
Lam <tsukiko852@gmail.com>
2019-04-26 03:03:59 +0800

    perfect version

--- Main.java
+++ Main.java
@@ -1,50 +1,86 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		List<String> stringList = new ArrayList<>();
-		System.out.println("type string until type 0");
-		String line = "";
-		while(!line.equals("0")) {
-			line = scanner.nextLine();
-			try {
-				Integer.parseInt(line);
-				System.out.println("you can't type a number!!");
-				continue;
-			}catch(NumberFormatException ignore) {
-			}
-			stringList.add(line);
-			System.out.println("you inserted "+line);
+	public static void main(String[] args) throws IOException {
+		LocalTime start = LocalTime.now();
+		System.out.println("Please enter file name: ");
+		String fileN = new Scanner(System.in).nextLine();
+		File file = new File(fileN);
+		if (!file.exists()) {
+			System.out.println("File not exist !");
+			return;
 		}
-		
-		stringList.forEach(System.out::println);
-		System.out.println("The result after analyse:");
-		anagram(stringList).forEach(System.out::println);
-		
+		Set<String> stringlist = new TreeSet<>();
+		FileReader fileReader = new FileReader(file);
+		BufferedReader reader = new BufferedReader(fileReader);
+		System.out.println("Processing.....");
+		System.out.println("Adding words to Cache...");
+		while (reader.ready()) {
+			String[] str = reader.readLine().split(" ");
+			for (String s : str) {
+				if (!s.isBlank())
+					stringlist.add(s.trim());
+			}
+		}
+		reader.close();
+		outputAlpha(stringlist);
+		output(anagram(stringlist));
+		LocalTime end = LocalTime.now();
+		Duration dur = Duration.between(start, end);
+		System.out.println("Process finished.");
+		System.out.println("Used Time : " + dur.toMinutes() + " mins");
 	}
-	
-	private static List<String> anagram(List<String> line){
-		LinkedList<String> results = new LinkedList<>();
+
+	private static void outputAlpha(Set<String> list) throws IOException {
+		File file = new File("alphabet.txt");
+		if (!file.exists()) file.createNewFile();
+		PrintWriter writer = new PrintWriter(new FileOutputStream(file));
+		ConcurrentLinkedDeque<String> queue = new ConcurrentLinkedDeque<>(list);
+		System.out.println("Outputting alphabet file.....");
+		while (!queue.isEmpty()) {
+			String line = queue.poll();
+			writer.println(line);
+		}
+		writer.close();
+	}
+
+
+	private static void output(Set<String> list) throws IOException {
+		File file = new File("output.txt");
+		if (!file.exists()) file.createNewFile();
+		PrintWriter writer = new PrintWriter(new FileOutputStream(file));
+		ConcurrentLinkedDeque<String> queue = new ConcurrentLinkedDeque<>(list);
+		System.out.println("Outputting file.....");
+		while (!queue.isEmpty()) {
+			String line = queue.poll();
+			String[] main = line.split(", ");
+			writer.println("Anagram words for " + main[0]);
+			writer.println(line);
+			writer.println("");
+			writer.println("");
+		}
+		writer.close();
+	}
+
+	private static Set<String> anagram(Set<String> line) {
+		Set<String> results = new TreeSet<>();
+		System.out.println("Sorting the words......");
 		for(String l : line){
 			char[] ch = l.toCharArray();
 			boolean equal = false, contains = false;
-			for(int i = 0; i < results.size(); i++) {
-				String liner = results.get(i);
+			for (String liner : results) {
 				char[] ch2 = liner.contains(",") ? liner.split(",")[0].toCharArray() : liner.toCharArray();
 				equal = equalCharArray(ch,ch2);
-				contains = results.get(i).contains(l);
+				contains = liner.contains(l);
 				if(equal) {
-					String original = results.get(i);
-					results.remove(i);
-					results.add(original+", "+l);
+					results.add(liner + ", " + l);
+					results.remove(liner);
 					break;
 				}
 			}
@@ -52,20 +88,19 @@ public class Main {
 		}
 		return results;
 	}
-	
-	private static List<Character> toList(char[] c){
-		List<Character> chars = new ArrayList<>();
-		for (int i = 0; i < c.length; i++) {
-			chars.add(c[i]);
+
+	private static Set<Character> toList(char[] c) {
+		Set<Character> chars = new TreeSet<>();
+		for (char c1 : c) {
+			if (c1 == ' ') continue;
+			chars.add(c1);
 		}
 		return chars;
 	}
 	
 	private static boolean equalCharArray(char[] ch, char[] ch2) {
-		List<Character> chlist = toList(ch);
-		List<Character> chlist2 = toList(ch2);
-		Collections.sort(chlist);
-		Collections.sort(chlist2);
-		return chlist.equals(chlist2);
+		Set<Character> chlist = toList(ch);
+		Set<Character> chlist2 = toList(ch2);
+		return chlist.size() == chlist2.size() && chlist.equals(chlist2);
 	}
 }
